c4b77f11edac28a41a693e71eed1f167ba3b025b
Greg <jakubowicz.gregory@gmail.com>
2022-05-10 19:33:37 +0300

    redux integration

--- tree-app/src/App.js
+++ tree-app/src/App.js
@@ -1,50 +1,26 @@
-import {useState, useEffect} from 'react';
-import {get} from './helpers/http';
-import Node from './components/Node';
+import {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchNodeRoot, fetchNodeChildrenById } from './features/nodes/nodesSlice'
+import Nodes from './components/Nodes';
 
 function App() {
 
-  const [root, setRoot] = useState(null)
+  const root = useSelector(state => state.nodes.root)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    get("http://127.0.0.1:5000/api/v1/nodes/root").then((root_data) => {
-      console.log("root data : ", root_data)
-      setRoot(root_data)
-    })
-  }, [])
+    dispatch(fetchNodeRoot())
+  }, [dispatch])
 
   const onNodeClick = (node) => {
-    if(node.isLoaded !== true && node.isLoading !== true) {
-      node.isLoading = true;
-      get("http://127.0.0.1:5000/api/v1/nodes/" + node.id + "/edges").then((edges) => {
-        console.log("edge data : ", edges)
-
-        setTimeout(() => {
-          node.children = [...edges];
-          node.isLoaded = true;
-          node.isOpened = true;
-
-          setRoot({...root})
-
-          node.isLoading = false;
-        }, 1000)
-      })
-    }
-
-    if(node.isLoaded === true) {
-      node.isOpened = !node.isOpened;
-      setRoot({...root})
-    }
+    dispatch(fetchNodeChildrenById(node))
   }
 
   console.log("render !")
 
   return (
-    <>
-    {root &&
-      <Node data={root} onNodeClick={onNodeClick} />
-    }
-    </>
+      <Nodes root={root} onNodeClick={onNodeClick} />
   );
 }
 
